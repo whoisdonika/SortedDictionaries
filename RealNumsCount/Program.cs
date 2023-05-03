7727ab60abb24a6f9305e574fe51afa885826683
Admin <Admin@28122017KP1>
2023-05-03 18:00:12 +0300

    Add project files.

---
+++ RealNumsCount/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealNumsCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] nums = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+            SortedDictionary<double, int> sortedNums = new SortedDictionary<double, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (sortedNums.ContainsKey(nums[i]))
+                {
+                    sortedNums[nums[i]]++;
+                }
+                else
+                {
+                    sortedNums.Add(nums[i], 1);
+                }
+            }
+            foreach (var item in sortedNums)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
---
+++ UpgradedPhonebook/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpgradedPhonebook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var phonebook = new Dictionary<string, string>();
+            var sortedPhonebook = new SortedDictionary<string, string>();
+
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split(' ').ToArray();
+
+                if (input[0] == "END")
+                {
+                    break;
+                }
+
+                if (input[0] == "A")
+                {
+                    string name = input[1];
+                    string phoneNum = input[2];
+
+                    phonebook[name] = phoneNum;
+                    sortedPhonebook[name] = phoneNum;
+                }
+
+                if (input[0] == "S")
+                {
+                    string name = input[1];
+                    if (phonebook.ContainsKey(name))
+                    {
+                        Console.WriteLine($"{name} -> {phonebook[name]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {name} does not exist.");
+                    }
+                }
+                if (input[0] == "ListAll")
+                {
+                    Console.WriteLine($"В указателя има {sortedPhonebook.Count} записани.");
+                    Console.WriteLine("Списък по азбучен ред :");
+                    foreach (var item in sortedPhonebook)
+                    {
+                        Console.WriteLine($"{item.Key} -> {item.Value}");
+                    }
+                }
+
+            }
+        }
+    }
+}
